06273142c7550e01a62090a043141cc317349aef
vonhoro <64147799+vonhoro@users.noreply.github.com>
2020-09-05 11:43:10 -0400

    Updates

---
+++ precache-manifest.d54112e7f66d546c27bb9ec0d48d8434.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "67fee793bcab2b8dc87b9e40e7c0c132",
+    url: "/DND-Board/index.html",
+  },
+  {
+    revision: "9959ec80b5e9ad59a43b",
+    url: "/DND-Board/static/css/2.fef7232c.chunk.css",
+  },
+  {
+    revision: "00085c6c350a919f9241",
+    url: "/DND-Board/static/css/main.9d9df25c.chunk.css",
+  },
+  {
+    revision: "9959ec80b5e9ad59a43b",
+    url: "/DND-Board/static/js/2.5db8ad9b.chunk.js",
+  },
+  {
+    revision: "3c71600f53edc413563dc5d496c182f4",
+    url: "/DND-Board/static/js/2.5db8ad9b.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "00085c6c350a919f9241",
+    url: "/DND-Board/static/js/main.fd486de0.chunk.js",
+  },
+  {
+    revision: "f6875eed9b7f80d75484",
+    url: "/DND-Board/static/js/runtime-main.2700982e.js",
+  },
+  {
+    revision: "a05e12e66d0697552934d0dafefa1a3a",
+    url: "/DND-Board/static/media/ApeBasicNeutral.a05e12e6.png",
+  },
+  {
+    revision: "237c04c58950708d1e8cda95585a943e",
+    url: "/DND-Board/static/media/BeeBasiNeutral.237c04c5.png",
+  },
+  {
+    revision: "286fe9dd3eccddec1473ce90fffc48b1",
+    url: "/DND-Board/static/media/GreenD.286fe9dd.png",
+  },
+  {
+    revision: "2a2b315e8cbf37013e488428712a05c1",
+    url: "/DND-Board/static/media/Monogo1.2a2b315e.png",
+  },
+  {
+    revision: "a05e12e66d0697552934d0dafefa1a3a",
+    url: "/DND-Board/static/media/abnb.a05e12e6.png",
+  },
+  {
+    revision: "d108845f8b485cfb7ceaf56d5cb4d88c",
+    url: "/DND-Board/static/media/aquenbe.d108845f.png",
+  },
+  {
+    revision: "237c04c58950708d1e8cda95585a943e",
+    url: "/DND-Board/static/media/bbnb.237c04c5.png",
+  },
+  {
+    revision: "68a3b9122d96b650342a5e13f57209ce",
+    url: "/DND-Board/static/media/hbnb.68a3b912.png",
+  },
+  {
+    revision: "68a3b9122d96b650342a5e13f57209ce",
+    url: "/DND-Board/static/media/humanBasicNeutral.68a3b912.png",
+  },
+  {
+    revision: "05f1cdadfe476395f60e233b15c22155",
+    url: "/DND-Board/static/media/icons-16.05f1cdad.eot",
+  },
+  {
+    revision: "3c1c220e7a18286503fb431c7a7fe183",
+    url: "/DND-Board/static/media/icons-16.3c1c220e.woff",
+  },
+  {
+    revision: "3cde8748332d1de6b1ae1c2dc5850754",
+    url: "/DND-Board/static/media/icons-16.3cde8748.ttf",
+  },
+  {
+    revision: "0a5c76518a68c185baa2c6744456918c",
+    url: "/DND-Board/static/media/icons-20.0a5c7651.eot",
+  },
+  {
+    revision: "51ec31f302d0072808e1f83f85fea4cd",
+    url: "/DND-Board/static/media/icons-20.51ec31f3.ttf",
+  },
+  {
+    revision: "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    url: "/DND-Board/static/media/icons-20.cef8cdbb.woff",
+  },
+  {
+    revision: "4b44d1695a47673cce4cee0ed27f32af",
+    url: "/DND-Board/static/media/monongo2png.4b44d169.png",
+  },
+  {
+    revision: "3824e57a2f469d860f6ab15367883a9a",
+    url: "/DND-Board/static/media/wbnb.3824e57a.png",
+  },
+]);
